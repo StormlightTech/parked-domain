e9a7afe1f70bd5928655f2ed696e3672e7fcd4d3
Felipe Sabino <felipe@sabino.me>
2015-09-04 12:01:17 -0700

    added node app and foreman

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
-parked-domain
+nodejs-parked-domain
 =============
 
-A simple minimalist html5 template for a parked domain
+A Node.js application to serve multiple parked domains.
+
+Layout from [rchrd2's minimalist html5 template](https://github.com/rchrd2/parked-domain)
 
 ## Screenshot
 ![Screenshot](/screenshot.png?raw=true "Screenshot")
+
+
+## Running
+
+```
+$ npm install
+$ npm start
+```
+
+## Options
+
+- Google analytics can be used and will be enabled by setting the `GA` environment variable.
---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var cons = require('consolidate');
+var app = express();
+
+// assign the jade engine to .jade files
+app.engine('jade', cons.jade);
+// set .jade as the default extension
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+
+app.get('*', function(req, res){
+  res.render('index', {
+    domain: req.hostname,
+    ga: process.env.GA
+  });
+});
+
+var port = process.env.PORT || 3000
+
+var server = app.listen(port, function () {
+  var port = server.address().port;
+
+  console.log('Example app listening at port %s', port);
+});
